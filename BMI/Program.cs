74fd311381586f829601759f7866e85c3f88febf
HuyTG200044 <huytgce200044@gmail.com>
2025-09-29 11:45:17 +0700

    BT BMI

---
+++ BMI/Program.cs
@@ -0,0 +1,21 @@
+﻿//Calculating BMI
+
+Console.Write("Enter your weight (kg): ");
+
+if (double.TryParse(Console.ReadLine(), out double w) && w > 0)
+{
+    Console.Write("Enter your height (m): ");
+    if (double.TryParse(Console.ReadLine(), out double h) && h > 0)
+    {
+        double BMI = w / (h * h);
+        Console.WriteLine($"Your BMI: {BMI:F2}");
+    }
+    else
+    {
+        Console.WriteLine("Invalid height, please check again!");
+    }
+}
+else
+{
+    Console.WriteLine("Invalid weight, please check again!");
+}
